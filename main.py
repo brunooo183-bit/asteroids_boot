73eba48f854af703c07bc21df9043f17360a5597
brunooo183-bit <brunooo183@gmail.com>
2026-09-14 22:19:38 +0300

    E2 : movement grouped

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable , drawable)
     ship = Player((SCREEN_WIDTH / 2) , (SCREEN_HEIGHT / 2))
     while True:
         log_state()
@@ -17,8 +20,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        ship.update(dt)
-        ship.draw(screen)
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000 
 if __name__ == "__main__":
