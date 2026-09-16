538f33caf7f8cd3dc1e172c76ccaf76961df7f08
brunooo183-bit <brunooo183@gmail.com>
2026-09-16 16:58:50 +0300

    H: asteroid destruction

--- main.py
+++ main.py
@@ -32,10 +32,16 @@ def main():
         screen.fill("black")
         updatable.update(dt)
         for asteroid in asteroids:
+            for shot in shots :
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
+        for asteroid in asteroids:
             if asteroid.collides_with(ship):
                 log_event("player_hit")
                 print("Game over!")
-                sys.exit()
+                sys.exit()        
         for thing in drawable:
             thing.draw(screen)
         pygame.display.flip()
